4956fc9e4a324f2d0dc8bc0227e288b328af25f2
mmbechezi <mlanawo.mbechezi@ikimea.com>
2011-12-10 01:49:43 +0100

    Refactoring

--- Resources/public/js/zones.js
+++ Resources/public/js/zones.js
@@ -1,32 +1,53 @@
-$(document).ready(function(){
-    
+$(function(){  
     var zone  = $('.zone').attr('rel');
 
     $.get('/page/zone/'+ zone + '/get' , function(data) {
-    
+
         $('.zone .content').html(data);
        
-         $('.zone .content .component').click(function(e){
-             
-            component_id = $(this).attr('rel');
-            $('<div>').dialog({
-                modal: true,
-                open: function ()
-                {
-                    $(this).load('/page/component/'+ component_id +'/edit');
-                },         
-                height: 180,
-                width: 600,
-                title: 'Ajax Page'
-            });
-               /* jQuery.facebox(function() {
-                    jQuery.get('/page/component/'+ component_id +'/edit', function(data) {
-                        jQuery.facebox(data );
-                    })
-                }); */
+         $('.zone .content .component .ikp-controls .edit').click(function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            
+            $('.ikp-controls').hide();
+            var component_id = $(this).attr('rel');
+            $('#component'+component_id + ' .ikp-content').load('/page/component/'+ component_id +'/edit');
+            $('#component-'+component_id).attr('style', 'width:650px; height: 300px;');
          });
        
+    });
+   
+    
+    $('form').submit(function(){
+        alert('Handler for .submit() called.');
+        return false;
     });
     
 });
 
+
+function ikpRemoteSubmit(thas){
+    var Btn = $(thas).find('.submit');
+    var iForm = $(thas);
+    var iFormURL = iForm.attr('action');
+    Btn.click(function(event){
+        event.preventDefault();
+        var update = $(thas).parent('.ikp-content');
+        console.log(update);
+        $.ajax({
+            type: 'POST',
+            url: iFormURL,
+            dataType: 'html',
+            data: iForm.serialize(),
+            success: function(data){
+                $(update).html(data);
+                $('.ikp-controls').hide();
+            }
+        });
+    });
+}
+
+ $('form input:submit').click(function(){
+       console.log('dd'); 
+ });
+    
